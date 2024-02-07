9748f0636ffec452bc86b1a38d218005e38d2980
Muhammed Easa <easanedumangad@gmail.com>
2024-02-07 23:18:54 +0530

    07-02-2024 night

---
+++ 349.js
@@ -0,0 +1,13 @@
+var intersection = function(nums1, nums2) {
+    let set1 = new Set(nums1)
+    let set2 = new Set(nums2)
+    let common = []
+    for(num of set1){
+        if(set2.has(num)){
+            common.push(num)
+        }
+    }
+    return common
+};
+ let nums1 = [1,2,2,1], nums2 = [2,2]
+console.log(intersection(nums1,nums2));
