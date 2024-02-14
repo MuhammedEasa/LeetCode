4f5743d432dd0156a2e21b350b2162b917b3a738
Muhammed Easa <easanedumangad@gmail.com>
2024-02-14 23:07:47 +0530

    14-02-2024 night

---
+++ 1047.js
@@ -0,0 +1,13 @@
+var removeDuplicates = function (s) {
+  let stack = [];
+  for (let char of s) {
+    if (stack[stack.length - 1] === char) {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+  return stack.join("");
+};
+let s = "abbaca";
+console.log(removeDuplicates(s));
